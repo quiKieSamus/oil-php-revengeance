a8ecf6fe1641e6fab257a3382b875cc4885793bb
Rubén González <gonzru01atwork@gmail.com>
2022-04-04 15:10:45 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let form = document.querySelectorAll('input');
+let arrPresion = [];
+let arrTiempo = [];
+
+let canGraphics = document.getElementById("graphics");
+new Chart("graphics", {
+     type: "bar",
+     data: {
+       labels: arrTiempo,
+       datasets:[{
+         fill: false,
+         lineTension: 0,
+         backgroundColor: "rgba(0, 0, 255, 1.0)",
+         borderColor: "rgba(0, 0, 255, 0.1)",
+         data: arrPresion
+       }]
+     },
+     options: {
+       legend: {display: false},
+       scales: {
+         yAxes: [{ticks: {min: 6, max:16}}]
+       }
+     }
+ });
+ 
+for(let i = 0; i < form.length; i++){
+  console.log(i);
+  let presion;
+  let tiempo;
+  if(document.getElementById("presion"+i).value != null) {
+    presion = document.getElementById("presion"+i).value;
+    tiempo = document.getElementById("tiempo"+i).value;
+    arrPresion.push(presion);
+    arrTiempo.push(tiempo);
+  } 
+}
+
+
+
+
--- php/list.php
+++ php/list.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php 
 
 include('config.php');
@@ -5,38 +6,41 @@ include('config.php');
 $query = "SELECT * FROM pozo";
 
 $result = mysqli_query($conn, $query);
-
+$i = 0;
 if(mysqli_num_rows($result) > 0) {
+    echo "
+    <table border='1' id='dTable'>
+    <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Presión</th>
+        <th>Tiempo de medición</th>
+        <th>Eliminar</th>
+        <th>Ver</th>
+    </tr>
+</table>
+    
+    ";
     while($row = mysqli_fetch_assoc($result)) {
+
         echo "
-        <table border='1'>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Presión</th>
-                <th>Tiempo de medición</th>
-                <th>Eliminar</th>
-                <th>Ver</th>
-            </tr>
 
-            <tr>
-                <form action='erase.php' method='get'>
-                    <td><input type='text' value='$row[id]' name='id' readonly></td>
-                    
-                    <td><input type='text' name='name' value='$row[nombre]' readonly></td>
-                    
-                    <td><input type='text' name='presion' value='$row[presion]' readonly>psi</td>
-
-                    <td><input type='text' name='time' value='$row[tiempo]' readonly></td>
-                    <td><input type='submit' value='eliminar'></td>
-                    
-                </form>
-                <td><input type='radio'></td>
-            </tr>
         
-        </table>";
+        
+        <form action='erase.php' method='get'>
+            <td><input type='text' value='$row[id]' name='id' readonly></td>
+            <td><input type='text' name='name' value='$row[nombre]' readonly></td>
+            <td><input type='text' name='presion' value='$row[presion]' id='presion$i' readonly>psi</td>
+            <td><input type='text' name='time' value='$row[tiempo]' id='tiempo$i' readonly></td>
+            <td><input type='submit' value='eliminar'></td>
+            <td><input type='radio'></td>        
+        
+        </form>";
+
+        
         
         // echo "id: " . $row["id"]. " - Nombre: " . $row["nombre"] . " - Presion: " . $row["presion"].  "psi<br>";
+        $i++;
     }
 } else {
     echo "0 results";
@@ -45,5 +49,21 @@ if(mysqli_num_rows($result) > 0) {
 
 ?>
 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <a href="../add.html">Añadir pozo</a>
 <a href="../index.html">Inicio</a>
+
+<canvas id="graphics" style="width: 100%; max-width: 700ṕx;"></canvas>
+<script src="../node_modules/chart.js/dist/chart.js">
+
+</script>
+<script src="../js/script.js"></script>
+</body>
+</html>
